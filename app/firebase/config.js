c3abf37604f0f520968e6e4fcf1cff301304d572
Danielkojo <danielkojo005@gmail.com>
2024-06-14 19:08:43 +0000

    insuranceclaimauthstart

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDCW9xVztBjdrc0i-t2ikXi0Dd8B9uBtQA",
+    authDomain: "insuranceclaim-9df4f.firebaseapp.com",
+    projectId: "insuranceclaim-9df4f",
+    storageBucket: "insuranceclaim-9df4f.appspot.com",
+    messagingSenderId: "344045429969",
+    appId: "1:344045429969:web:4f3c2da9408449f5d111f0",
+    measurementId: "G-8T1KKMSZTG"
 };
 
 // Initialize Firebase
